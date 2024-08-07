409e47a8436bd5cc3dbfc82552064d62eb0d9b25
ZeroHzzzz <3190381602@qq.com>
2024-08-08 01:51:53 +0800

    fix:修复user路由未注册的问题

--- gateway/app.py
+++ gateway/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
-from flask import Blueprint
 from api.routes import api
 from api.bus.routes import bus
+from api.user.routes import user
 
 app = Flask(__name__)
 app.register_blueprint(api, url_prefix='/api')
 
 app.register_blueprint(bus, url_prefix='/api/bus')
+
+app.register_blueprint(user, url_prefix='/api/user')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
